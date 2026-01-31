d91a3c7762e46cd625a1d50caf1ef13c6b5d3013
RedAISkye <tandukarbidhan@gmail.com>
2026-01-31 07:29:09 +0545

    Initial Release.

---
+++ CostumesUnlocker.cs
@@ -0,0 +1,53 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace RedAISkye.WickedSeed.CostumesUnlocker
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class CostumesUnlocker : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+            var harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(PlayerData), "GetOwnedCostumes")]
+    public static class PlayerData_GetOwnedCostumes_Patch
+    {
+        private static bool costumeNamelogged = false;
+        public static bool Prefix(ref List<Costume> __result, PlayerData __instance)
+        {
+            FieldInfo dbField = typeof(PlayerData).GetField("dbCostumes", BindingFlags.NonPublic | BindingFlags.Instance);
+            if (dbField == null)
+            {
+                CostumesUnlocker.Logger.LogError("dbCostumes field not found in PlayerData!");
+                return true;
+            }
+
+            var dbCostumes = dbField.GetValue(__instance);
+            MethodInfo getAllMethod = dbCostumes.GetType().GetMethod("GetAll");
+            __result = (List<Costume>)getAllMethod.Invoke(dbCostumes, null);
+
+            if (!costumeNamelogged)
+            {
+                costumeNamelogged = true;
+
+                string allCostumes = string.Join(", ", __result.ConvertAll(c => c.name));
+                CostumesUnlocker.Logger.LogInfo($"Total Costumes: {__result.Count}");
+                CostumesUnlocker.Logger.LogInfo($"Unlocked Costumes: {allCostumes}");
+            }
+
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+﻿# Wicked Seed – Costumes Unlocker By RedAISkye
+
+<img width="1280" height="640" alt="Wicked Seed - Costumes Unlocker (Git)" src="https://repository-images.githubusercontent.com/1143139813/f3fb5125-4341-42f7-883f-b6c151646499" />
+
+---
+
+## Mod Official Page
+https://smol.tandukarbidhan.com.np/ShopWSCosUnMod
+
+---
+
+## 1. What does this mod do?
+> This mod unlocks all costumes right from the start without requiring any completion of challenges/shop purchases.
+
+## 2. How do I install this mod?
+> Extract the contents directly inside the game's installed directory.
+
+## 3. Can I preview a screenshot of how the mod looks?
+> Of course, there is a screenshot that demonstrates the mod perfectly called **"Preview.png"**.
+
+## 4. How do I uninstall this mod?
+> Delete the folder **"BepInEx"** and files **"doorstop_config.ini"**, **"winhttp.dll"** & **"Preview.png"**.
+
+## 5. Can I use this mod for whatever purpose I want?
+> Yeah, just make sure to credit me for this mod by my name **"RedAISkye"** with the link for this mod provided here and **Dead Right Games** for developing this cool game!
+
+<img width="1075" height="2697" alt="Wicked Seed - Costumes Unlocker (Preview)" src="https://tandukarbidhan.com.np/wp-content/uploads/2026/01/Wicked-Seed-Costumes-Unlocker-Preview-scaled.png" />
+
+---
+
+## Changelog
+
+### v1.1
+> Fix Console Logging for debugging purposes.
+
+### v1.0
+> Initial Release.
+
+---
+
+https://tandukarbidhan.com.np
+
+---
